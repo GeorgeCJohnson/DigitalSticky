53cc7cb623559eb0316f28c16110d9af0e79baf3
George J <Gjohnson.pioneer@gmail.com>
2023-10-03 22:56:24 -0400

    Change the error throw code in utils file.

--- utils/utils.js
+++ utils/utils.js
@@ -3,19 +3,30 @@ const { resolve } = require("path");
 
 // Promisify the fs.readFile method
 const readFileAsync = (filePath, encoding) => {
+  return new Promise ((resolve, reject) => {
     fs.readFile(filePath, encoding, (err, data) => {
-        if (err) throw err;
-        console.log(data);
-      }); 
-}
+      if (err) {
+        reject(err);
+      } else {
+        resolve(data);
+      }
+    });
+  });
+};
 
 // Promisify the fs.writeFile method
-const writeFileAsync = (filePath, data, encoding) => {
-    fs.writeFile(filePath, data, encoding, (err) => {
-        if (err) throw err;
+const writeFileAsync = (filePath, data) => {
+  return new Promise ((resolve, reject) => {
+    fs.writeFile(filePath, data, (err) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve();
+        }
         console.log("The file has been saved!");
-      }); 
-}
+    }); 
+  });
+}  
 
 module.exports = {readFileAsync, writeFileAsync};
 
